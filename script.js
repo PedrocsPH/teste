5d6f45592b5a71744218fe700fe3741dc402a851
Pedro Henrique Tavares de Andrade <60176035079@senacrs.edu.br>
2024-08-16 11:13:58 -0300

    Aprendendo Javascript
    Trabalhamos com console, alerta e prompt com base em Javascript.
    
    Co-Authored-By: Sandro Martins da Costa <sandromartinscosta@gmail.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+// perguntando ao usuario
+let nome = prompt("digite seu nome");
+alert("Olá "+nome+", tudo bem?");
+
+// alerta na tela
+alert("Kong bom dia!");
+
+// console do navegador
+console.log("Teste de console");
+
+// inclui um html em um elemento identificado pelo ID
+document.getElementById("demo").innerHTML = "Pedro Henrique";
+
+// Dado
+let num = Math.random();
+alert(num);
